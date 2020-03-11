c287d2d0631bf460ed414e2c7c4f7f30b59a7ffb
xuzhenlu <13103615283@163.com>
2020-03-11 15:29:48 +0800

    修改执行时间

--- COV/getDataFile.js
+++ COV/getDataFile.js
@@ -50,8 +50,8 @@ async function dealTargetHtml(data){
 }
 // 每隔1小时执行一次
 let rules = new schedule.RecurrenceRule();
-// 每小时50分执行
-rules.minute = 50
+// 每小时35分执行
+rules.minute = 35
 console.log("开始等待执行")
 schedule.scheduleJob(rules,function(){
     console.log("程序已执行",new Date().getMinutes())
